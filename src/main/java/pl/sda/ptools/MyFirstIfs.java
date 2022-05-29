ff6c228390e1880ecd4ff6e472a3207dc5debe1d
Mateusz <Mateuszochal9310@gmail.comgit commit -m innitial>
2022-05-29 14:16:09 +0200

    poprawione komentarze

---
+++ src/main/java/pl/sda/ptools/MyFirstIfs.java
@@ -0,0 +1,22 @@
+package pl.sda.ptools;
+
+import java.util.Scanner;
+
+public class MyFirstIfs {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter number A: ");
+        int a = scanner.nextInt();
+        System.out.print("Please enter number B: ");
+        int b = scanner.nextInt();
+
+        if (a > b) {
+            System.out.println("A is greater than B");
+        } else if (a < b) {
+            System.out.println("A is lesser than B");
+        } else {
+            System.out.println("A is equal to B");
+        }
+    }
+}
